e81edeb80fb0d8dbbbd20b20e77cc2882f6c0888
Lray143 <Lray.gulferic@gmail.com>
2026-01-26 10:00:26 +0800

    Added front to the character box

--- MartialAscension/js/states/matchMulti.js
+++ MartialAscension/js/states/matchMulti.js
@@ -9,7 +9,8 @@ let p1 = {
   crouchH: 185,
   h: 350,
   velY: 0,
-  lastInput: ""
+  lastInput: "",
+  facing: 1 // 1 = right, -1 = left
 };
 
 let p2 = {
@@ -20,7 +21,8 @@ let p2 = {
   crouchH: 185,
   h: 350,
   velY: 0,
-  lastInput: ""
+  lastInput: "",
+  facing: -1 // 1 = right, -1 = left
 };
 
 let speed = 8;
@@ -48,43 +50,25 @@ function drawMatchMulti() {
   // ================================
   let p1Inputs = [];
 
-  // Movement
   if (keyIsDown(65)) { p1.x -= speed; p1Inputs.push("A"); }
   if (keyIsDown(68)) { p1.x += speed; p1Inputs.push("D"); }
+  if (keyIsDown(87) && p1.y + p1.h >= groundY) { p1.velY = -jumpPower; p1Inputs.push("W"); }
 
-  // Jump
-  if (keyIsDown(87) && p1.y + p1.h >= groundY) {
-    p1.velY = -jumpPower;
-    p1Inputs.push("W");
-  }
-
-  // Crouch / Stand
   if (keyIsDown(83)) {
-    if (p1.h !== p1.crouchH) {
-      p1.y += p1.h - p1.crouchH;
-      p1.h = p1.crouchH;
-    }
+    if (p1.h !== p1.crouchH) { p1.y += p1.h - p1.crouchH; p1.h = p1.crouchH; }
     p1Inputs.push("S");
   } else {
-    if (p1.h !== p1.standH) {
-      p1.y += p1.h - p1.standH;
-      p1.h = p1.standH;
-    }
+    if (p1.h !== p1.standH) { p1.y += p1.h - p1.standH; p1.h = p1.standH; }
   }
 
-  // Attacks
-  if (keyIsDown(89)) p1Inputs.push("LP"); // Y
-  if (keyIsDown(85)) p1Inputs.push("HP"); // U
-  if (keyIsDown(72)) p1Inputs.push("LK"); // H
-  if (keyIsDown(74)) p1Inputs.push("HK"); // J
+  if (keyIsDown(89)) p1Inputs.push("LP");
+  if (keyIsDown(85)) p1Inputs.push("HP");
+  if (keyIsDown(72)) p1Inputs.push("LK");
+  if (keyIsDown(74)) p1Inputs.push("HK");
 
-  // Gravity
   p1.velY += gravity;
   p1.y += p1.velY;
-  if (p1.y + p1.h > groundY) {
-    p1.y = groundY - p1.h;
-    p1.velY = 0;
-  }
+  if (p1.y + p1.h > groundY) { p1.y = groundY - p1.h; p1.velY = 0; }
 
   p1.lastInput = p1Inputs.join(" + ");
 
@@ -95,50 +79,63 @@ function drawMatchMulti() {
 
   if (keyIsDown(LEFT_ARROW)) { p2.x -= speed; p2Inputs.push("←"); }
   if (keyIsDown(RIGHT_ARROW)) { p2.x += speed; p2Inputs.push("→"); }
-
-  if (keyIsDown(UP_ARROW) && p2.y + p2.h >= groundY) {
-    p2.velY = -jumpPower;
-    p2Inputs.push("↑");
-  }
+  if (keyIsDown(UP_ARROW) && p2.y + p2.h >= groundY) { p2.velY = -jumpPower; p2Inputs.push("↑"); }
 
   if (keyIsDown(DOWN_ARROW)) {
-    if (p2.h !== p2.crouchH) {
-      p2.y += p2.h - p2.crouchH;
-      p2.h = p2.crouchH;
-    }
+    if (p2.h !== p2.crouchH) { p2.y += p2.h - p2.crouchH; p2.h = p2.crouchH; }
     p2Inputs.push("↓");
   } else {
-    if (p2.h !== p2.standH) {
-      p2.y += p2.h - p2.standH;
-      p2.h = p2.standH;
-    }
+    if (p2.h !== p2.standH) { p2.y += p2.h - p2.standH; p2.h = p2.standH; }
   }
 
-  // Attacks
-  if (keyIsDown(73)) p2Inputs.push("LP"); // I
-  if (keyIsDown(79)) p2Inputs.push("HP"); // O
-  if (keyIsDown(75)) p2Inputs.push("LK"); // K
-  if (keyIsDown(76)) p2Inputs.push("HK"); // L
+  if (keyIsDown(73)) p2Inputs.push("LP");
+  if (keyIsDown(79)) p2Inputs.push("HP");
+  if (keyIsDown(75)) p2Inputs.push("LK");
+  if (keyIsDown(76)) p2Inputs.push("HK");
 
-  // Gravity
   p2.velY += gravity;
   p2.y += p2.velY;
-  if (p2.y + p2.h > groundY) {
-    p2.y = groundY - p2.h;
-    p2.velY = 0;
-  }
+  if (p2.y + p2.h > groundY) { p2.y = groundY - p2.h; p2.velY = 0; }
 
   p2.lastInput = p2Inputs.join(" + ");
+
+  // ================================
+  // FACE EACH OTHER
+  // ================================
+  if (p1.x < p2.x) { p1.facing = 1; p2.facing = -1; }
+  else { p1.facing = -1; p2.facing = 1; }
 
   // ================================
   // DRAW PLAYERS
   // ================================
   noStroke();
+
+  // Player 1
   fill(255, 80, 80);
   rect(p1.x, p1.y, p1.w, p1.h);
 
+  // Front indicator (triangle sticks to front)
+  fill(255, 200, 200);
+  let frontH = p1.h * 0.2; // dynamic to height
+  let frontY = p1.y + p1.h / 2;
+  if (p1.facing === 1) {
+    triangle(p1.x + p1.w, frontY - frontH / 2, p1.x + p1.w, frontY + frontH / 2, p1.x + p1.w + frontH, frontY);
+  } else {
+    triangle(p1.x, frontY - frontH / 2, p1.x, frontY + frontH / 2, p1.x - frontH, frontY);
+  }
+
+  // Player 2
   fill(80, 120, 255);
   rect(p2.x, p2.y, p2.w, p2.h);
+
+  fill(200, 200, 255);
+  let frontH2 = p2.h * 0.2;
+  let frontY2 = p2.y + p2.h / 2;
+  if (p2.facing === 1) {
+    triangle(p2.x + p2.w, frontY2 - frontH2 / 2, p2.x + p2.w, frontY2 + frontH2 / 2, p2.x + p2.w + frontH2, frontY2);
+  } else {
+    triangle(p2.x, frontY2 - frontH2 / 2, p2.x, frontY2 + frontH2 / 2, p2.x - frontH2, frontY2);
+  }
 
   // ================================
   // DRAW NAMES
@@ -146,14 +143,9 @@ function drawMatchMulti() {
   textAlign(CENTER, BOTTOM);
   textSize(24);
   fill(255);
-
   if (typeof FIGHTERS !== "undefined") {
-    if (FIGHTERS[p1Selected]) {
-      text(FIGHTERS[p1Selected].name, p1.x + p1.w / 2, p1.y - 10);
-    }
-    if (FIGHTERS[p2Selected]) {
-      text(FIGHTERS[p2Selected].name, p2.x + p2.w / 2, p2.y - 10);
-    }
+    if (FIGHTERS[p1Selected]) text(FIGHTERS[p1Selected].name, p1.x + p1.w / 2, p1.y - 10);
+    if (FIGHTERS[p2Selected]) text(FIGHTERS[p2Selected].name, p2.x + p2.w / 2, p2.y - 10);
   }
 
   // ================================
