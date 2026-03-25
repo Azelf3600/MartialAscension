641237576f8dca0aa5dfab6059acc910aade0db4
Azelf3600 <151857500+Azelf3600@users.noreply.github.com>
2026-03-25 11:20:54 +0800

    adding aaron sprites

--- MartialAscension/js/systems/animationSystem.js
+++ MartialAscension/js/systems/animationSystem.js
@@ -280,8 +280,27 @@ class AnimationSystem {
         let path = `Assets/sprites/characters/${folderName}/DivingKick/frame${frameNum}.png`;
         this.loadedAnimations[characterName].divingkick.push(loadImage(path));
       }
+      this.loadedAnimations[characterName].hiteffect = [];
+      for (let i = 0; i <= 12; i++) {
+        let frameNum = i.toString().padStart(4, '0');
+        let path = `Assets/sprites/characters/${folderName}/HitEffect/frame${frameNum}.png`;
+        this.loadedAnimations[characterName].hiteffect.push(loadImage(path));
+      }
     }
-    if (folderName === "AaronShu") { }
+    if (folderName === "AaronShu") { 
+      this.loadedAnimations[characterName].idle = [];
+      for (let i = 0; i <= 12; i++) {
+        let frameNum = i.toString().padStart(4, '0');
+        let path = `Assets/sprites/characters/${folderName}/Idle/frame${frameNum}.png`;
+        this.loadedAnimations[characterName].idle.push(loadImage(path));
+      }
+      this.loadedAnimations[characterName].crouch = [];
+      for (let i = 0; i <= 12; i++) {
+        let frameNum = i.toString().padStart(4, '0');
+        let path = `Assets/sprites/characters/${folderName}/Crouch/frame${frameNum}.png`;
+        this.loadedAnimations[characterName].crouch.push(loadImage(path));
+      }
+    }
     if (folderName === "DamonCheon") { }
   }
 
